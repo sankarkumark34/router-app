44ab80e384c86f4cc9103a29e13fffcd2ab1fd74
sankarkumar K <sankarkumar.k@c1exchange.com>
2022-07-27 11:54:03 +0530

    #login page with jwt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
-const routes: Routes = [{ path: 'connect', loadChildren: () => import('./modules/connect/connect.module').then(m => m.ConnectModule) }, 
+const routes: Routes = [
+{path:'',component:LoginComponent},
+{ path: 'connect', loadChildren: () => import('./modules/connect/connect.module').then(m => m.ConnectModule) }, 
 { path: 'segment', loadChildren: () => import('./modules/segment/segment.module').then(m => m.SegmentModule) },
 { path: 'shared', loadChildren: () => import('./modules/shared/shared.module').then(m => m.SharedModule) }];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
 import {CalendarModule} from 'primeng/calendar';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './modules/login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import{HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
     SharedModule,
-    CalendarModule
+    CalendarModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[CalendarModule],
   providers: [],
